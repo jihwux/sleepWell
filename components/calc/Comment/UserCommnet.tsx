00f048ccbe2c0cc991fdcae7c681ea5176fa555f
Hwan97 <hg_07@naver.com>
2021-04-27 23:49:26 +0900

    test: Add test components

---
+++ components/calc/Comment/UserCommnet.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserCommnet = () => {
+  return <div></div>;
+};
+
+export default UserCommnet;
--- components/calc/News/SleepInfo.tsx
+++ components/calc/News/SleepInfo.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+
+// 건강 뉴스 받기 , 이후 상단 본인 건강 글 상단 독점.
+
+const NewsList = () => {};
 
 const SleepInfo = () => {
-  return <div>티스토리 불러오기.</div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=2f0afaba1dab40bdaec56e27691e9a6a'
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={onClick}> hi </Button>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 };
 
 export default SleepInfo;
+// 2f0afaba1dab40bdaec56e27691e9a6a
