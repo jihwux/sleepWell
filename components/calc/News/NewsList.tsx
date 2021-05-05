969fac8f14bb1c764acd3d395fcdd94d6619222f
Hwan97 <hg_07@naver.com>
2021-05-05 23:59:10 +0900

    fix: News api change as naver

--- components/calc/News/NewsList.tsx
+++ components/calc/News/NewsList.tsx
@@ -1,51 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
-import NewsItem from './NewsItem';
-
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-  category: 'health',
-};
+import { NextApiResponse, NextApiRequest } from 'next';
+// import NewsItem from './NewsItem';
 
 const NewsList = () => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState();
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     //async
     const fetchData = async () => {
-      setLoading(true);
+      const client_id = 'DEEVJGU0hVQnOySMiJN7';
+      const client_secret = 'SMlXxOWhtD';
+      // setLoading(true);
       try {
+        // const response = await axios.get(
+        //   'https://newsapi.org/v2/everything?q=불면증&apiKey=2f0afaba1dab40bdaec56e27691e9a6a'
+        // );
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&category=health&apiKey=2f0afaba1dab40bdaec56e27691e9a6a'
-          // category: 'health'
+          'https://openapi.naver.com/v1/search/news.json',
+          {
+            params: {
+              query: '코로나',
+              display: 20,
+            },
+            headers: {
+              'X-Naver-Client-Id': client_id,
+              'X-Naver-Client-Secret': client_secret,
+              // 'Access-Control-Allow-Origin': '*',
+            },
+          }
         );
-        setArticles(response.data.articles);
+        // setArticles(response.data.articles);
+        // console.log(articles);
+        // console.log(response);
+        // console.log('sdf');
       } catch (e) {
         console.log(e);
       }
-      setLoading(false);
+      // setLoading(false);
+      const tests = response.data;
+      console.log(tests);
+      return {
+        props: { tests },
+      };
     };
     fetchData();
   }, []);
 
-  if (loading) {
-    return <div>대기중</div>;
-  }
+  // if (loading) {
+  //   return <div>대기중</div>;
+  // }
 
-  if (!articles) {
-    return null;
-  }
+  // if (!articles) {
+  //   return null;
+  // }
 
   return (
     <div>
-      {articles.map((article) => (
+      {/* {articles.map((article) => (
         <NewsItem key={article.url} article={article} />
-      ))}
+      ))} */}
+      sdfsdfsdf
     </div>
   );
 };
--- pages/calc/index.tsx
+++ pages/calc/index.tsx
@@ -6,38 +6,4 @@ const index: NextPage = () => {
   return <Calc />;
 };
 
-// index.getInitialProps = async ({ store, query }) => {
-//   const {
-//     checkInDate,
-//     checkOutDate,
-//     adultCount,
-//     childrenCount,
-//     latitude,
-//     longitude,
-//     limit,
-//     page = "1",
-//   } = query;
-//   try {
-//     const { data } = await getRoomListAPI({
-//       checkInDate,
-//       checkOutDate,
-//       adultCount,
-//       childrenCount,
-//       latitude,
-//       longitude,
-//       limit: limit || "20",
-//       page: page || "1",
-//       //? 한글은 encode해주세요.
-//       location: query.location
-//         ? encodeURI(query.location as string)
-//         : undefined,
-//     });
-//     store.dispatch(roomActions.setRooms(data));
-//   } catch (e) {
-//     console.log(e);
-//   }
-
-//   return {};
-// };
-
 export default index;
