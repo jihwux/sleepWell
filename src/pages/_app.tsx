2d519f175d586fe2d2e6f19983723257d7ce3fe8
Hwan97 <hg_07@naver.com>
2022-01-12 03:56:52 +0900

    Add meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Header from '../components/common/Header';
 import { GlobalStyle } from '../styles/global-styles';
 import { MuiCustomStyle } from '../styles/muiCustom';
 import { lightTheme, darkTheme, Theme } from '../styles/theme';
-
+import { NextSeo } from 'next-seo';
 import { useDarkMode } from '../hooks/useDarkMode';
 
 interface ContextProps {
@@ -25,10 +25,31 @@ const app = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <>
+        <NextSeo
+          title="꿀잠"
+          titleTemplate="꿀잠"
+          defaultTitle="꿀잠"
+          description="바쁜 당신을 위한 수면 계산기"
+          canonical="https://www.sleepfit.site/"
+          openGraph={{
+            url: 'https://www.sleepfit.site/',
+            title: '꿀잠',
+            description: '바쁜 당신을 위한 수면 계산기. ',
+            images: [
+              {
+                url: '../public/appStore.png',
+                width: 800,
+                height: 420,
+                alt: '꿀잠',
+              },
+            ],
+          }}
+        />
         <GlobalStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
         <MuiCustomStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
 
         <Header />
+
         <Component {...pageProps} />
       </>
     </ThemeContext.Provider>
