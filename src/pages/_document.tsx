8510cfa8abc182cf3d765983405ace1ecb5cc522
Hwan97 <hg_07@naver.com>
2022-01-15 03:47:34 +0900

    fix: favicon display

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,16 +30,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/public/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
-          <link rel="manifest" href="/public/site.webmanifest" />
+          <link rel="icon" href=".//favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href=".//apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href=".//favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href=".//favicon-16x16.png" />
+          <link rel="manifest" href=".//site.webmanifest" />
           <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap" rel="stylesheet" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet" />
-          <meta name="msapplication-TileColor" content="#ffc40d" />
-          <meta name="theme-color" content="#ffffff" />
+          {/* <meta name="msapplication-TileColor" content="#ffc40d" /> */}
+          {/* <meta name="theme-color" content="#ffffff" /> */}
           <meta name="naver-site-verification" content="25fd5a14fddc8f6f9013c5a78efe50f4339053ec" />
           <meta name="google-site-verification" content="j8oqjfHaWtzRdXV1dGHj-j47ttB4wYUwRnIGDirPLrw" />
 
