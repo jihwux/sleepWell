cd4948322a59b8f9d448d910850223383dd41ae4
Hwan97 <hg_07@naver.com>
2022-02-11 13:48:03 +0900

    fix: theme transition error #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import Header from '../components/common/Header';
 import { GlobalStyle } from '../styles/global-styles';
 import { MuiCustomStyle } from '../styles/muiCustom';
@@ -22,45 +22,50 @@ export const ThemeContext = createContext<ContextProps>({
 
 const app = ({ Component, pageProps }: AppProps) => {
   const { theme, toggleTheme } = useDarkMode();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <>
-        <Head>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <NextSeo
-          title="꿀잠"
-          description="바쁜 당신을 위한 수면 계산기"
-          canonical="https://sleepcycle.tech/"
-          openGraph={{
-            type: 'website',
-            url: 'https://sleepcycle.tech/',
-            title: '꿀잠',
-            description: '바쁜 당신을 위한 수면 계산기',
-            images: [
-              {
-                url: '/images/brandThum.png',
-                width: 600,
-                height: 300,
-                alt: '램수면 걔산기',
-                type: 'image/png',
-              },
-            ],
-            site_name: '꿀잠',
-          }}
-          twitter={{
-            handle: '@handle',
-            site: '@site',
-            cardType: 'summary_large_image',
-          }}
-        />
-        <GlobalStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
-        <MuiCustomStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
-        <Header />
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <NextSeo
+        title="꿀잠"
+        description="바쁜 당신을 위한 수면 계산기"
+        canonical="https://sleepcycle.tech/"
+        openGraph={{
+          type: 'website',
+          url: 'https://sleepcycle.tech/',
+          title: '꿀잠',
+          description: '바쁜 당신을 위한 수면 계산기',
+          images: [
+            {
+              url: '/images/brandThum.png',
+              width: 600,
+              height: 300,
+              alt: '램수면 걔산기',
+              type: 'image/png',
+            },
+          ],
+          site_name: '꿀잠',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <GlobalStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
+      <MuiCustomStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
 
-        <Component {...pageProps} />
-      </>
+      <Header />
+      <Component {...pageProps} />
     </ThemeContext.Provider>
   );
 };
--- src/pages/calc/index.tsx
+++ src/pages/calc/index.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Calc from '../../components/calc/Calc';
 
 const index: NextPage = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return <Calc />;
 };
 
