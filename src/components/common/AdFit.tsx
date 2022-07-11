a0f0fb23d07f7ef9f6f45711508e906ba66f514e
Hwan97 <hg_07@naver.com>
2022-07-11 17:43:48 +0900

    adfit

--- src/components/common/AdFit.tsx
+++ src/components/common/AdFit.tsx
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
 export function AdFit() {
-  // let ins = document.createElement('ins');
-  // let scr = document.createElement('script');
+  let ins = document.createElement('ins');
 
-  // // ins.className = 'kaka'
-  // // scr.async = true;
-  // // scr.type = 'text/javascript';
-  // // scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
-  // // ins.setAttribute('data-ad-width', '728');
-  // // ins.setAttribute('data-ad-height', '90');
-  // // ins.setAttribute('data-ad-unit', 'DAN-hJh9Obf3OtboDnTl');
+  ins.className = 'kakao_ad_area';
+  useEffect(() => {
+    let scr = document.createElement('script');
+    scr.async = true;
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    // ins.setAttribute('data-ad-width', '728');
+    // ins.setAttribute('data-ad-height', '90');
+    // ins.setAttribute('data-ad-unit', 'DAN-hJh9Obf3OtboDnTl');
+    // document.querySelector('.adfit').appendChild(ins);
+
+    const myDom = document.querySelector('.adfit') as HTMLParagraphElement;
+    myDom?.appendChild(scr);
+    return () => {};
+  }, []);
 
   return (
-    <div style={{ overflow: 'hidden', minWidth: '300px', minHeight: '250px' }}>
+    <div className="adfit" style={{ overflow: 'hidden', minWidth: '300px', minHeight: '250px' }}>
       <ins
         className="kakao_ad_area"
         style={{ display: 'none', width: '100%' }}
